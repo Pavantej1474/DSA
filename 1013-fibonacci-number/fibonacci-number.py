3f98c7ae90cb27db117d002089b0f0721ae84712
Pavantej1474 <97780533+Pavantej1474@users.noreply.github.com>
2025-09-27 11:53:57 +0530

    Time: 357 ms (19.97%) | Memory: 17.5 MB (98.76%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -6,8 +6,8 @@ class Solution:
         if n<2:
             return n
         else:
-            a = self.fib(n-1) + self.fib(n-2)
+            return self.fib(n-1) + self.fib(n-2)
         
-        return a
+        
 
         
